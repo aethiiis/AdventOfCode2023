5c68a5b43905bb2f6df7b4bbcd2508c3727ce939
aethiiis <pubalexpub@gmail.com>
2023-12-14 18:00:15 +0100

    changement de pc

---
+++ src/day14/day14_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"advent_of_code_2023/src/utilities"
+	"testing"
+)
+
+func TestPart1Example(t *testing.T) {
+	path1 := "input_day14_test"
+	lines1 := utilities.ReadLines(path1)
+
+	res1 := Part1(lines1)
+	exp1 := 405
+	if res1 != exp1 {
+		t.Errorf("Mauvaise valeur : on s'attendait à %d mais on a obtenu %d.\n", exp1, res1)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	path := "input_day14"
+	lines := utilities.ReadLines(path)
+	res := Part1(lines)
+	exp := 43614
+	if res != exp {
+		t.Errorf("Mauvaise valeur : on s'attendait à %d mais on a obtenu %d.\n", exp, res)
+	}
+}
+func TestPart2Example(t *testing.T) {
+	path3 := "input_day14_test"
+	lines3 := utilities.ReadLines(path3)
+
+	res3 := Part2(lines3)
+	exp3 := 400
+	if res3 != exp3 {
+		t.Errorf("Mauvaise valeur : on s'attendait à %d mais on a obtenu %d.\n", exp3, res3)
+	}
+}
+func TestPart2(t *testing.T) {
+	path := "input_day14"
+	lines := utilities.ReadLines(path)
+
+	res := Part2(lines)
+	exp := 36771
+	if res != exp {
+		t.Errorf("Mauvaise valeur : on s'attendait à %d mais on a obtenu %d.\n", exp, res)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	path := "input_day14"
+	lines := utilities.ReadLines(path)
+
+	for i := 0; i < b.N; i++ {
+		Part1(lines)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	path := "input_day14"
+	lines := utilities.ReadLines(path)
+
+	for i := 0; i < b.N; i++ {
+		Part2(lines)
+	}
+}
---
+++ src/day14/part1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"advent_of_code_2023/src/utilities"
+)
+
+func Part1(lines []string) int {
+	var tab [][]rune
+	var poids int
+	poids = 0
+	tab = utilities.ConvertirListeStringsTableauRunes(lines)
+	for i := range tab {
+		for j := range tab[i] {
+			if tab[i][j] == 'O' {
+				poids += len(lines) - chute(&tab, i, j)
+			}
+		}
+	}
+	/*fmt.Println("Voici tab : ")
+	for i := range tab {
+		fmt.Println(string(tab[i]))
+	}*/
+	return poids
+}
+
+func chute(tab *[][]rune, i, j int) int {
+	if (*tab)[i][j] != 'O' {
+		panic("Appel non-valide")
+	}
+	finalLine := i
+	for k := i - 1; k >= 0; k-- {
+		if (*tab)[k][j] == '.' {
+			finalLine--
+		} else {
+			break
+		}
+	}
+	if finalLine != i {
+		(*tab)[i][j] = '.'
+		(*tab)[finalLine][j] = 'O'
+	}
+
+	return finalLine
+}

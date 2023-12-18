ae39ea09f5cfa0caa2775159dbf8bafa014f8ac9
aethiiis <pubalexpub@gmail.com>
2023-12-18 11:23:50 +0100

    javascript function

--- src/day1/day1part1.js
+++ src/day1/day1part1.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
 
-const content = fs.readFileSync('input_day1', 'utf-8')
-const lines = content.split('\n')
-let digits = []
-Array.from({length: lines.length}, (v, i) => {
-    const line = lines[i]
-    let digit = ""
-    Array.from({length: line.length}, (v, j) => {
-        if (!isNaN(parseInt(line[j]))) {
-            digit += line[j]
-        }
+function part1() {
+    const content = fs.readFileSync('input_day1', 'utf-8')
+    const lines = content.split('\n')
+    let digits = []
+    Array.from({length: lines.length}, (v, i) => {
+        const line = lines[i]
+        let digit = ""
+        Array.from({length: line.length}, (v, j) => {
+            if (!isNaN(parseInt(line[j]))) {
+                digit += line[j]
+            }
+        })
+        digits.push(parseInt(digit[0] + digit[digit.length-1]))
     })
-    digits.push(parseInt(digit[0] + digit[digit.length-1]))
-})
-const sum = digits.reduce((acc, cur) => acc + cur, 0)
-console.log(sum)
-module.exports = sum
+    const sum = digits.reduce((acc, cur) => acc + cur, 0)
+    console.log(sum)
+}
+part1()
+//module.exports = sum
